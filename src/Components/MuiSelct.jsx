9f8961a8b9d54864513f1c8b9de586dd4430e01f
keyur <keyur.tecmantras1@gmail.com>
2022-09-27 14:05:07 +0530

    NA

--- src/Components/MuiSelct.jsx
+++ src/Components/MuiSelct.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Box, MenuItem, TextField } from "@mui/material";
+import { React, useState } from "react";
 
 const MuiSelct = () => {
-  return (
-    <div>MuiSelct</div>
-  )
-}
+  const [countries, setCountries] = useState([]);
+  console.log({ countries });
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
+  };
 
-export default MuiSelct
+  return (
+    <>
+      <Box width="250px">
+        <TextField
+          label="Select Country"
+          select
+          value={countries}
+          onChange={handleChange}
+          fullWidth
+          SelectProps={{
+            multiple: true,
+          }}
+          size="small"
+          helperText="Select Country"
+          color="secondary"
+        >
+          <MenuItem value="IN">India</MenuItem>
+          <MenuItem value="US">USA</MenuItem>
+          <MenuItem value="AU">Ausrelia</MenuItem>
+        </TextField>
+      </Box>
+    </>
+  );
+};
+
+export default MuiSelct;
